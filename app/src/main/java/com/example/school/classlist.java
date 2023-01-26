a6e6d5a14e60bc2a21317aae7986261de68f47de
Avishek kumar <avishekiitism@gmail.com>
2023-01-26 16:47:15 +0530

    Firebase fetching student details

--- app/src/main/java/com/example/school/classlist.java
+++ app/src/main/java/com/example/school/classlist.java
@@ -2,13 +2,28 @@ package com.example.school;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class classlist extends AppCompatActivity {
+
+    Button btn6,btn7,btn8,btn9,btn10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classlist);
+
+        btn6 = findViewById(R.id.btn6);
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(classlist.this, subjectpdflist.class);
+                startActivity(intent);
+            }
+        });
     }
 }
